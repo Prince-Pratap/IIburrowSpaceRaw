bb95ea38c79a348273321f56675b2373721b36d0
Prince Pratap <128032381+Prince-Pratap@users.noreply.github.com>
2024-09-25 03:17:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,4 +50,5 @@ def handle_new_peer(data):
         print("Received new peer connection without an ID.")
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
